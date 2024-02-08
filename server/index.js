e478680aafb79ffdfe5ca832c3bb3438e7545fb9
Shital6377 <shital.ebiztrait@gmail.com>
2024-02-08 12:33:15 +0530

    add a simple project

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const EmployeeModel = require('./models/Employee')
+
+const app = express()
+app.use(cors(
+    {
+        origin: ["http://localhost:5174"], // https://mern-user-create.vercel.app
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.use(express.json())
+
+const dbUrl =  "mongodb+srv://sarmistha1:sarmistha@cluster0.npn1qmh.mongodb.net/store";
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(dbUrl, connectionParams)
+
+
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
+
+app.post('/login',(req,res)=>{
+    const{email,password}=req.body;
+    EmployeeModel.findOne({email: email})
+    .then(user=>{
+        if(user){
+            if(user.password === password){
+                res.json('success')
+            }
+            else{
+                res.json('The password is incorrect')
+            }
+        } else{
+            res.json('No record existed')
+        }
+    })
+})
+
+app.post('/register',(req,res)=>{
+    EmployeeModel.create(req.body)
+    .then(employee => res.json(employee))
+    .catch(err => res.json(err))
+})
+
+
+app.listen(3002, () => {
+    console.log("Server is Running on port", 3002)
+})
